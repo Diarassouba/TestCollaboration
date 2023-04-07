cdbc76c76d0271618edd265858b248f1a68098c2
Diarassouba <diaramadou471@gmail.com>
2023-04-07 02:05:55 +0200

    demo

---
+++ script.js
@@ -0,0 +1,11 @@
+const form = document.querySelector('.form-quizz')
+let tableauResultats = [];
+const reponses = ['c','a','b','a','c'];
+const emojis = ['✔️','✨','👀','😭','👎'];
+const titreResultat = document.querySelector('.resultats h2');
+const noteResultat = document.querySelector('.note');
+const aidesResultat = document.querySelector('.aide');
+const toutesLesQuestions = document.querySelectorAll('.question-block');
+
+
+let verifTableau = [];
